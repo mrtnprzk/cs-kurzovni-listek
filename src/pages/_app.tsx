09fd4d1b1ee1b0b147ec97bd3b22b37fe4a2225c
mrtnprzk <marti.parizek@gmail.com>
2023-05-23 22:35:14 +0200

    [UPDATE] Head title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import { DataProvider } from "@/context/DataContext";
 
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <DataProvider>
-            <Component {...pageProps} />
-        </DataProvider>
+        <>
+            <Head>
+                <title>ČS - Kurzovní lístek</title>
+            </Head>
+            <DataProvider>
+                <Component {...pageProps} />
+            </DataProvider>
+        </>
     );
 }
